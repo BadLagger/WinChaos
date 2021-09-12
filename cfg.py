fb09ffee2d67d4ce1e41c9bf1d706c54adabf03a
BadLagger <lanynamer1985@gmail.com>
2021-09-12 03:37:21 +0300

    Add icon for app

--- cfg.py
+++ cfg.py
@@ -12,18 +12,19 @@ class Cfg:
 
 
     def get_language(self):
-        if self.__init == True:
-            return self.__sets.get("lang")
-        else:
-            return None
+        return self.__get_prm('lang')
 
     def get_filter(self):
-        if self.__init == True:
-            return self.__sets.get("filter")
-        else:
-            return None
+        return self.__get_prm('filter')
+
+    def get_trayicon(self):
+        return self.__get_prm('icontray')
+
+    def __get_prm(self, prm_name):
+        return self.__sets.get(prm_name) if self.__init == True else None
 
 if __name__ == "__main__":
     cfg = Cfg("sets.cfg")
     print(cfg.get_language())
     print(cfg.get_filter())
+    print(cfg.get_trayicon())
